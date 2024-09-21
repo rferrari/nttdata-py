d6049cd1dda90580cba406beed9adebab8586812
RFerrari <rferrariwd@gmail.com>
2024-09-21 00:27:12 -0300

    feat: sep

--- a4-input.py
+++ a4-input.py
@@ -1,6 +1,6 @@
 input("whats your name? ")
 
-name = input("whats your name? ")
+name = input("sorry, whats your name? ")
 print("helo "+ name)
 
 age = input("whats your age? ")
@@ -10,6 +10,7 @@ print(name, age)
 
 print(age, name, end=" ")
 print(age, name, end="...\n")
+print(age, name, "Age: "+ age, "Name: "+ name, sep=" -- ", end="...\n")
 print("bye bye")
 
 

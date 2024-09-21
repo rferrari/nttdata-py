3941fca87f928be04bdd658a28c9d12e0e5e8811
adam <adam@kali>
2024-09-21 16:26:39 -0300

    feat: atribuicoes

---
+++ a5-atribuicao.py
@@ -0,0 +1,31 @@
+number1 = 10
+number2 = 20
+number3 = 5
+number4 = 5
+
+print(number1, number2, number3, number4)
+
+#print(f"{number1} == {number2} "+ str(number1 == number2))
+
+#print(number1)
+
+number1 += number4
+print(number1)
+
+number1 -= 1
+print(number1)
+
+number1 *= number3
+print(number1)
+
+number1 /= 2
+print(number1)
+
+number1 %= 2
+print(number1)
+
+number1 *= number2
+print(number1)
+
+number1 **= 3
+print(int(number1))

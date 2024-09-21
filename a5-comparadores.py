ca1f2ab4962614e2e3b6ca5970c45176a28eddd5
adam <adam@kali>
2024-09-21 16:14:32 -0300

    feat: comparadores

---
+++ a5-comparadores.py
@@ -0,0 +1,28 @@
+number1 = 10
+number2 = 20
+number3 = 5
+number4 = 5
+
+print(number1, number2, number3, number4)
+
+print(f"{number1} == {number2}")
+print(number1 == number2)
+
+print(f"{number3} == {number4}")
+print(number3 == number4)
+
+print(f"{number3} > {number4}")
+print(number3 > number4)
+
+print(f"{number1} < {number4}")
+print(number1 < number4)
+
+print(f"{number3} >= {number4}")
+print(number3 >= number4)
+
+print(f"{number1} != {number2}")
+print(number1 != number2)
+
+print(f"{number3} != {number4}")
+print(number3 != number4)
+

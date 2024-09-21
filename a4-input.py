9c2bbf70e580a666f87b1101a7ea1f970208779e
RFerrari <rferrariwd@gmail.com>
2024-09-21 00:25:01 -0300

    feat: input print end

---
+++ a4-input.py
@@ -0,0 +1,19 @@
+input("whats your name? ")
+
+name = input("whats your name? ")
+print("helo "+ name)
+
+age = input("whats your age? ")
+print(name + " has "+ age)
+
+print(name, age)
+
+print(age, name, end=" ")
+print(age, name, end="...\n")
+print("bye bye")
+
+
+
+
+
+
